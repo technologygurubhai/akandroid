8d5e24b2a21edd41418ba8035a34c29f3d90e200
technologygurubhai <67548913+technologygurubhai@users.noreply.github.com>
2022-09-21 09:52:41 -0700

    Create notification.js

---
+++ notification.js
@@ -0,0 +1,13 @@
+<script src="https://cdn.onesignal.com/sdks/OneSignalSDK.js" async=""></script>
+<script>
+  window.OneSignal = window.OneSignal || [];
+  OneSignal.push(function() {
+    OneSignal.init({
+      appId: "3ae9afe2-2f01-4a69-af8e-64ed82cc4f6b",
+      safari_web_id: "web.onesignal.auto.1bb493cc-6f2b-4253-90e1-02d85a4b9e78",
+      notifyButton: {
+        enable: true,
+      },
+    });
+  });
+</script>
